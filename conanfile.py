2ef14b10c2cf254c0fbf9f0db4ab4367215e3cc0
Rutger ter Borg <rutger.terborg@artibit.com>
2018-07-24 11:12:59 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,40 @@
+ 
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+import shutil
+import os
+
+class LibodbConan( ConanFile ):
+    name = "libodb"
+    version = "2.4.0"
+    license = "GPL"
+    url = "https://github.com/barcharcraz/conan-packages"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = "shared=False", "fPIC=True"
+
+    def source(self):
+        tools.get( "https://www.codesynthesis.com/download/odb/2.4/libodb-2.4.0.tar.bz2", sha1="42bd2a8023e338e004711e755eb30bd122b844a6" )
+
+    def source_path( self ):
+        return os.path.join( self.source_folder, self.name + '-' + self.version )
+
+    def build( self ):
+        
+        env_build = AutoToolsBuildEnvironment(self)
+        env_build.fpic = self.options.fPIC
+
+        configure_args = []
+        if not self.options.shared:
+            configure_args.extend( [ '--enable-static', '--disable-shared', '--enable-static-boost' ] )
+        
+        env_build.configure( configure_dir = self.source_path(), args=configure_args )
+        env_build.make()
+
+    def package(self):
+        
+        self.copy( "*.hxx", dst="include/odb", src= os.path.join( self.source_path(), "odb" )  )
+        self.copy( "*.a", dst="lib", keep_path=False )
+
+    def package_info(self):
+        self.cpp_info.libs = ["libodb"]
+
